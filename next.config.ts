bad294e6ad4d0a7c12e2147d1f1c17707716eeeb
Lourrane Xavier <lourrannyvick@gmail.com>
2026-04-06 19:19:07 -0300

    Fix: remove invalid config and update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Isso resolve os avisos de bloqueio de IP na sua rede local
-  experimental: {
-    allowedDevOrigins: ['192.168.56.1', 'localhost:3000'],
-  },
-  // Melhora a performance de carregamento de imagens externas se necessário
+   
   images: {
-    formats: ['image/avif', 'image/webp'],
+  formats: ['image/avif', 'image/webp'],
   },
 };
 
